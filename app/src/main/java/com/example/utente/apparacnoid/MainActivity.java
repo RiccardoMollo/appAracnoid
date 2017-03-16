54f415078676fb5814b63293d6f14dd6ba1fa5dd
RiccardoMollo <s230082@studenti.polito.it>
2017-03-16 15:47:39 +0100

    salvattaggio settings

--- app/src/main/java/com/example/utente/apparacnoid/MainActivity.java
+++ app/src/main/java/com/example/utente/apparacnoid/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.utente.apparacnoid;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
@@ -16,8 +17,10 @@ public class MainActivity extends AppCompatActivity {
 
     EditText nome_et;
     Button scegli_colore;
+    Button avvia;
     int[] lista_colori= new int[]{Color.argb(255,200,0,0),Color.argb(255,0,255,0),Color.argb(255,0,0,255)};
     int i=0;
+    int colore=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
         nome_et=(EditText) findViewById(R.id.nome_et);
         scegli_colore=(Button) findViewById(R.id.scegli_colore);
+        avvia = (Button) findViewById(R.id.inizia);
+
 
         }
+
     public void change(View view) {
-        scegli_colore.getBackground().setColorFilter(lista_colori[i], PorterDuff.Mode.SRC_ATOP);
+        //scegli_colore.getBackground().setColorFilter(lista_colori[i], PorterDuff.Mode.SRC_ATOP);
+        colore = lista_colori[i];
+        scegli_colore.getBackground().setTint(colore);
         i++;
         if(i==3){
             i=0;
@@ -37,10 +45,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void start (View view){
+        SharedPreferences settings = getSharedPreferences("Settings", 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("nome",nome_et.getText().toString() );
+        editor.apply();
+        editor.putInt("colore",colore);
+        editor.apply();
+
         Intent intent = new Intent (this, Livello.class);
         startActivity(intent);
     }
-    }
+
+}
 
 
 
