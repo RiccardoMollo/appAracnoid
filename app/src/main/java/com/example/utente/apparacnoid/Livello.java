4bc55f20ea73ea9a9f7b961b6f5ea7926b262b12
Riccardo <riccardo.husse@gmail.com>
2017-03-16 15:15:18 +0100

    Creata seconda acitivty

---
+++ app/src/main/java/com/example/utente/apparacnoid/Livello.java
@@ -0,0 +1,13 @@
+package com.example.utente.apparacnoid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Livello extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livello);
+    }
+}
--- app/src/main/java/com/example/utente/apparacnoid/MainActivity.java
+++ app/src/main/java/com/example/utente/apparacnoid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.utente.apparacnoid;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +15,6 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     EditText nome_et;
-    Button inizia;
     Button scegli_colore;
     int[] lista_colori= new int[]{Color.argb(255,200,0,0),Color.argb(255,0,255,0),Color.argb(255,0,0,255)};
     int i=0;
@@ -25,24 +25,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         nome_et=(EditText) findViewById(R.id.nome_et);
-        inizia=(Button) findViewById(R.id.inizia);
         scegli_colore=(Button) findViewById(R.id.scegli_colore);
 
+        }
+    public void change(View view) {
+        scegli_colore.getBackground().setColorFilter(lista_colori[i], PorterDuff.Mode.SRC_ATOP);
+        i++;
+        if(i==3){
+            i=0;
+        }
+    }
 
-        scegli_colore.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View view) {
-
-
-                scegli_colore.getBackground().setColorFilter(lista_colori[i], PorterDuff.Mode.SRC_ATOP);
-                i++;
-                if(i==3){
-                    i=0;
-                }
-            }
-        });
+    public void start (View view){
+        Intent intent = new Intent (this, Livello.class);
+        startActivity(intent);
+    }
     }
 
 
-}
+
