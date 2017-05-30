0b42fc393849dd84e5a96dc02484328687605599
RiccardoMollo <s230082@studenti.polito.it>
2017-05-30 15:20:13 +0200

    classifica prova

---
+++ app/src/main/java/com/example/utente/apparacnoid/Giocatore.java
@@ -0,0 +1,33 @@
+package com.example.utente.apparacnoid;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Created by Utente on 30/05/2017.
+ */
+
+public class Giocatore implements Comparable <Giocatore> {
+    String nome;
+    int punteggio;
+
+    public Giocatore(String n, int p){
+        this.nome=n;
+        this.punteggio=p;
+    }
+
+    public int getPunteggio() {
+        return punteggio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public int compareTo(Giocatore giocatoreComp) {
+        int comparePunteggio = ((Giocatore) giocatoreComp).getPunteggio();
+
+        //ascending order
+        return this.punteggio - comparePunteggio;
+    }
+}
--- app/src/main/java/com/example/utente/apparacnoid/RisultatiFinali.java
+++ app/src/main/java/com/example/utente/apparacnoid/RisultatiFinali.java
@@ -7,10 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class RisultatiFinali extends AppCompatActivity {
 
     int risultato;
     TextView risultati_tv;
+    String nomeGiocatore;
+    Giocatore giocatore;
+    ArrayList<Giocatore> listaGiocatori;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +30,51 @@ public class RisultatiFinali extends AppCompatActivity {
         SharedPreferences settings = getSharedPreferences("Settings", 0);
         risultati_tv = (TextView) findViewById(R.id.risultatiFinali);
         risultato = settings.getInt("punteggioFinale",0);
-        risultati_tv.setText("Punteggio finale: " + risultato);
+        nomeGiocatore = settings.getString("nome","");
+        risultati_tv.setText("Punteggio finale di " + nomeGiocatore + " : " + risultato);
+        giocatore=new Giocatore(nomeGiocatore,risultato);
+        listaGiocatori = getListaGiocatori(settings);
+        listaGiocatori.add(giocatore);
+        Collections.sort(listaGiocatori);
+        provaLista(listaGiocatori);
+        saveListaGiocatori(settings,listaGiocatori);
+
+
+
+
+    }
+
+    private void saveListaGiocatori(SharedPreferences settings,ArrayList listagiocatori) {
+
+        SharedPreferences.Editor prefsEditor = settings.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(listagiocatori);
+        prefsEditor.putString("listaGiocatori", json);
+        prefsEditor.apply();
+    }
+
+    private void provaLista(ArrayList<Giocatore> listaGiocatori) {
+        if(listaGiocatori.size()!=0){
+            String sequenzaNomi="";
+            for(int i=0; i<listaGiocatori.size();i++){
+                sequenzaNomi = sequenzaNomi + listaGiocatori.get(i).getNome();
+            }
+            risultati_tv.setText(sequenzaNomi);
+        }
+    }
+
+    private ArrayList<Giocatore> getListaGiocatori(SharedPreferences settings) {
+        ArrayList <Giocatore> listaG;
+        Gson gson = new Gson();
+        String json = settings.getString("listaGiocatori", "");
+        if (json.isEmpty()) {
+            listaG = new ArrayList<Giocatore>();
+        } else {
+            Type type = new TypeToken<List<Giocatore>>() {
+            }.getType();
+            listaG = gson.fromJson(json, type);
+        }
+        return listaG;
     }
 
     public void replay(View view) {
